46e092a7d9de8437f23f9fa75f3d4e113561eec2
Q3333 <50946901+Q3333@users.noreply.github.com>
2019-10-07 17:07:19 +0900

    Revert "jsp_radar"

--- rb-spring/src/main/java/com/review/wiki/HelloController.java
+++ rb-spring/src/main/java/com/review/wiki/HelloController.java
@@ -2,28 +2,14 @@ package com.review.wiki;
 
 import java.util.Date;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 	
 	@GetMapping("/api/hello")
 	public String hello() {
 		return "Hello, the time at the server is now " + new Date() + "\n";
 	}
-	
-//    @RequestMapping("/test")
-//    public String test() throws Exception {
-//        return "test";
-//    }
-//    
-//    @RequestMapping("/test2")
-//    public String abcd() throws Exception {
-//        return "test2";
-//    }
-    
 }
